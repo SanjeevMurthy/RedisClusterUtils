4d2470e4c75079608eb33f67addbbe715f473575
Sanjeev Murthy <sanjeevmurthy01@gmail.com>
2024-10-09 02:31:52 +0530

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -2,7 +2,5 @@ module.exports = function() {
     return config = {
     Cluster: [],
     
-   // sentinalName : 'debtestmaster' ,
-    //sentinalDetails = 'DVMTGDRDIS001UK.dev.global.tesco.org', '30001', 'DVMTGDRDIS001UK.dev.global.tesco.org', '30002', 'DVMTGDRDIS001UK.dev.global.tesco.org', '30003'  
-}
+  }
 };
