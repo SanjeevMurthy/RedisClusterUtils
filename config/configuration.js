5eb218add6a32c2ee83ff9fe4ec8855e8434862a
Sanjeev Murthy <sanjeevmurthy01@gmail.com>
2024-10-09 02:31:31 +0530

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,11 +1,6 @@
 module.exports = function() {
     return config = {
-    Cluster: [{ host: 'DVMTGDRDIS001UK.dev.global.tesco.org', port: 3001 },
-                { host: 'DVMTGDVNSH001UK.dev.global.tesco.org', port: 3002 },
-                { host: 'DVMTGDVNSH001UK.dev.global.tesco.org', port: 3003 },
-                { host: 'DVMTGDVNSH001UK.dev.global.tesco.org', port: 3004 },
-                { host: 'DVMTGDVNSH001UK.dev.global.tesco.org', port: 3005 },
-                { host: 'DVMTGDVNSH001UK.dev.global.tesco.org', port: 3006 }],
+    Cluster: [],
     
    // sentinalName : 'debtestmaster' ,
     //sentinalDetails = 'DVMTGDRDIS001UK.dev.global.tesco.org', '30001', 'DVMTGDRDIS001UK.dev.global.tesco.org', '30002', 'DVMTGDRDIS001UK.dev.global.tesco.org', '30003'  
